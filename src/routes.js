7875220244e72e86e8554da677888d6158ecfdd1
tgulmine <tgulmine@gmail.com>
2023-04-23 20:53:59 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -16,27 +16,35 @@ export const routes = [
                 title: search
             } : null)
 
-            return res.end(JSON.stringify(tasks))
+            if (!tasks) {
+                return res.writeHead(401).end("Sem tasks.")
+            }
+
+            return res.writeHead(200).end(JSON.stringify(tasks))
         }
     },
     {
         method: 'POST',
         path: buildRoutePath('/tasks'),
         handler: (req, res) => {
-            const { title, description, completed_at, created_at, updated_at } = req.body
+            const { title, description } = req.body
+
+            if (!title || !description) {
+                return res.writeHead(401).end("Necessário conter campos title e description.")
+            }
 
             const task = {
                 id: randomUUID(),
                 title,
                 description,
-                completed_at,
-                created_at,
-                updated_at,
+                completed_at: null,
+                created_at: new Date(),
+                updated_at: new Date(),
             }
 
             database.insert('tasks', task)
 
-            return res.writeHead(201).end()
+            return res.writeHead(201).end(`Task criada com id ${task.id}`)
         }
     },
     {
@@ -44,17 +52,23 @@ export const routes = [
         path: buildRoutePath('/tasks/:id'),
         handler: (req, res) => {
             const { id } = req.params
-            const { title, description, completed_at, created_at, updated_at } = req.body
+            const { title, description } = req.body
+
+            if (!database.validateId('tasks', id)) {
+                return res.writeHead(401).end(`Id ${id} não encontrado.`)
+            }
+
+            if (!title || !description) {
+                return res.writeHead(401).end("Necessário conter campos title e description.")
+            }
 
             database.update('tasks', id, {
                 title,
                 description,
-                completed_at,
-                created_at,
-                updated_at,
+                updated_at: new Date(),
             })
 
-            return res.writeHead(204).end()
+            return res.writeHead(204).end(`Task com id ${id} foi alterada.`)
         }
     },
     {
@@ -63,9 +77,13 @@ export const routes = [
         handler: (req, res) => {
             const { id } = req.params
 
+            if (!database.validateId('tasks', id)) {
+                return res.writeHead(401).end(`Id ${id} não encontrado.`)
+            }
+
             database.delete('tasks', id)
 
-            return res.writeHead(204).end()
+            return res.writeHead(204).end(`Task com id ${id} foi deletada.`)
         }
     },
     {
@@ -74,17 +92,16 @@ export const routes = [
         handler: (req, res) => {
             const { id } = req.params
 
-            //completed at current time
+            if (!database.validateId('tasks', id)) {
+                return res.writeHead(401).end(`Id ${id} não encontrado.`)
+            }
 
             database.update('tasks', id, {
-                title,
-                description,
-                completed_at,
-                created_at,
-                updated_at,
+                completed_at: new Date(),
+                updated_at: new Date(),
             })
 
-            return res.writeHead(204).end()
+            return res.writeHead(204).end(`Task com id ${id} foi marcada como completa.`)
         }
     }
 ]
